d94d32289d262769b0ec7467fab2943cd164dfea
tonghaidang2310 <121488408+tonghaidang2310@users.noreply.github.com>
2024-07-01 10:40:49 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,6 @@
 import joblib
 import os
 
-def create_classifier():
-    from sklearn.naive_bayes import MultinomialNB
-    return MultinomialNB(alpha=1.0)
-
 def save_model(model, model_path, model_name='model.joblib'):
     if not os.path.exists(model_path):
         os.makedirs(model_path)
